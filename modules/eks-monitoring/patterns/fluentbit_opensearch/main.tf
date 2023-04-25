d5aaf0b67e46be74c72a57c8eb017e5a4c63c964
Lucas Vieira <lvieiras@amazon.com>
2023-04-25 19:57:09 +0000

    Fix kubernetes authentication issue

--- modules/eks-monitoring/patterns/fluentbit_opensearch/main.tf
+++ modules/eks-monitoring/patterns/fluentbit_opensearch/main.tf
@@ -5,14 +5,22 @@ data "aws_eks_cluster" "this" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_id]
+    command     = "aws"
+  }
 }
 
 provider "helm" {
   kubernetes {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+    exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_id]
+    command     = "aws"
+  }
   }
 }
 
